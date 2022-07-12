8ce734316eea38a294619d3775fb5e1777eb146b
Владислав <trupackz@gmailcom>
2022-07-12 22:59:55 +0500

    домашка к 5 семинару исправленная версия 2

--- Zadacha_36/Program.cs
+++ Zadacha_36/Program.cs
@@ -6,7 +6,7 @@ int[] GetArray(int size)
     int[] array = new int[size];
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(-999, 999);
+        array[i] = new Random().Next(100);
     }
     return array;
 }
@@ -21,7 +21,7 @@ int SumElemOddPosition(int[] array)
     int sum = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 != 0)
+        if (i % 2 == 1)
         {
             sum += array[i];
         }
@@ -31,4 +31,4 @@ int SumElemOddPosition(int[] array)
 
 int[] array = GetArray(20);
 PrintArray(array);
-System.Console.WriteLine($"Сумма нечетных элементов: {SumElemOddPosition(array)}");
+System.Console.WriteLine($"Сумма элементов на нечетных позициях индекса: {SumElemOddPosition(array)}");
