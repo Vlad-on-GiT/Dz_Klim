86c248077ad32ba98df9ecc3b60c799536360206
Владислав <trupackz@gmailcom>
2022-07-15 20:34:46 +0500

    домашка к 6 семинару

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Red;
+System.Console.WriteLine(" ------------------------------------------");
+System.Console.WriteLine("|         Вариант через ConvertAll         |");
+System.Console.WriteLine("|              и используя LINQ            |");
+System.Console.WriteLine(" ------------------------------------------");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+System.Console.WriteLine("Введите числа через запятую: ");
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+
+int[] arrPositiveInd = arr
+    .Where ((item, Index) => item > 0 )
+    .ToArray();
+
+System.Console.WriteLine("Чисел больше 0 в массиве: {0}", arrPositiveInd.Length);
---
+++ Zadacha_43/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+
+Console.WriteLine("Введите координаты A, B через запятую для первой прямой:");
+double[] firstArr = StrToArray();
+Console.WriteLine("Введите координаты A, B через запятую для второй прямой:");
+double[] secondArr = StrToArray();
+
+FindIntersection(firstArr, secondArr);
+
+void FindIntersection(double[] firstArr, double[] secondArr)
+{
+    if (firstArr[0] == secondArr[0])
+    {
+        if (firstArr[0] == secondArr[0] && firstArr[1] == secondArr[1]) Console.WriteLine("Прямые совпадают");
+        if (firstArr[0] != secondArr[0] && firstArr[1] == secondArr[1]) Console.WriteLine("Прямые совпадают");
+        else Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        double x = (firstArr[0] - secondArr[0]) / (secondArr[1] - firstArr[1]);
+        double y = firstArr[1] * x + firstArr[0];
+        System.Console.WriteLine($"Точки пересечения прямых  ({x};{y}).");
+    }
+
+
+}
+
+double[] StrToArray()
+{
+    return Console.ReadLine()
+        .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(Double.Parse).ToArray();
+}
+
