27273af2a34774773c6c631f3e33452ef64d6589
Владислав <trupackz@gmailcom>
2022-07-24 17:06:33 +0500

    домашка к 8 семинару, задачи 64 и 66

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+void SumNum(int M, int N, int sum = 0)
+{
+    if (M == N)
+    {
+        System.Console.WriteLine(sum + M);
+        return;
+    }
+    SumNum(M + 1, N, sum + M);
+}
+
+SumNum(1, 15);
---
+++ Zadacha_66/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+
+m = 2, n = 3 -> A(m,n) = 9 */
+
+int Akkerman(int m, int n)
+{
+    int temp = 0;
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        temp = Akkerman(m - 1, 1);
+        return temp;
+    }
+    else if (m > 0 && n > 0)
+    {
+        temp = Akkerman(m - 1, Akkerman(m, n - 1));
+        return temp;
+    }
+    return 0;
+}
+
+System.Console.WriteLine(Akkerman(2, 3));
